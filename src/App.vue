af5a5acfb2d2f24e8597674ed30efaf85c76df4a
Sitent Web <75006755+sitentweb@users.noreply.github.com>
2022-09-19 00:54:39 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>New To CodeSandbox</h1>
+    <NewComp />
+  </div>
+</template>
+
+<script>
+import NewComp from "./components/NewComp";
+export default {
+  name: "App",
+  components: {
+    NewComp,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NewComp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button class="new-button">New Button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewComp",
+};
+</script>
+
+<style>
+.new-button {
+  padding: 15px 20px;
+  border: none;
+  background-color: blueviolet;
+  color: white;
+  border-radius: 5px;
+  font-size: 15px;
+  font-weight: 200;
+  transition: 0.4s;
+}
+
+.new-button:hover {
+  box-shadow: 0px 5px 15px 1px rgba(0, 0, 0, 0.2);
+}
+</style>
